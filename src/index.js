2194a642e47225bbc5845b216cf496781b2218a5
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-04-24 23:22:09 +0200

    Add style.css

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function component() {
    // Lodash, currently included via a script, is required for this line to work
    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
  
     return element;
 }

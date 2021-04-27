93bf6087a64727efe87b85d6c342567b25a8eb7e
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-04-27 20:55:44 +0200

    Create functions for each countdown

--- src/index.js
+++ src/index.js
@@ -22,8 +22,7 @@ function component() {
 
     return element;
 }
- 
-  //document.body.appendChild(component());
+//document.body.appendChild(component());
 
 // Elements of DOM
 const $work = document.querySelector("#work");
@@ -32,3 +31,52 @@ const $start = document.querySelector("#start");
 const $pause = document.querySelector("#pause");
 const $reset = document.querySelector("#reset");
 const $cycleNb = document.querySelector("#cycleNb"); 
+// chrono
+let startminWork = 30;
+let timeWork = startminWork * 60;
+let startminRest = 5;
+let timeRest = startminRest * 60;
+
+// Countdown for Work
+const countdownWork = () => {
+  
+  const min = Math.floor(timeWork / 60);
+  let sec = timeWork % 60;
+
+  sec = sec < 10 ? '0' + sec : sec;
+
+  $work.innerHTML = `${min} : ${sec}`;
+
+  if(timeWork > 0){
+    timeWork --;
+  } else {
+    $work.innerHTML = `30 : 00`;
+    setInterval(countdownRest, 1000);
+  }
+	
+}
+
+// Countdown for Rest
+const countdownRest = () => {
+  
+  const min = Math.floor(timeRest / 60);
+  let sec = timeRest % 60;
+
+  sec = sec < 10 ? '0' + sec : sec;
+
+  $rest.innerHTML = `${min} : ${sec}`;
+
+  if(timeRest > 0){
+    timeRest --;
+  } else {
+    $rest.innerHTML = `05 : 00`;
+    setInterval(countdownWork, 1000);
+  }
+	
+}
+
+$start.addEventListener("click", () => {
+  setInterval(countdownWork, 1000);
+  $start.disabled = "true";
+});
+
